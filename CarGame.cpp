79c3000def269001ef175f1b5d08c94aa90b7eac
00300 <90952986+well300@users.noreply.github.com>
2022-01-31 00:06:47 +0530

    🍳

---
+++ CarGame.cpp
@@ -0,0 +1,261 @@
+/*
+Q.console ?
+A.A console is an operating system window through which a user can communicate with the operating system or we can say a console is an application in which we can give text as an input from the keyboard and get the text as an output from the computer end.
+Q.buffer ?
+A.A buffer is a data area shared by hardware devices or program processes that operate at different speeds or with different sets of priorities. The buffer allows each device or process to operate without being held up by the other. ... This term is used both in programming and in hardware.
+*/
+
+#include <iostream>
+/*
+-iostream is the header file which contains all the functions of program like cout, cin etc. and #include tells the preprocessor to include these header file in the program.
+Q.What is the cout in C++?
+A.The cout object in C++ is an object of class ostream.It is defined in iostream header file. It is used to display the output to the standard output device i.e. monitor. ... The data needed to be displayed on the screen is inserted in the standard output stream (cout) using the insertion operator(<<).
+Q.What is Cin C++ ?
+A.The cin object in C++ is an object of class iostream. It is used to accept the input from the standard input device i.e. keyboard. ... The extraction operator(>>) is used along with the object cin for reading inputs. The extraction operator extracts the data from the object cin which is entered using the keyboard.
+*/
+#include<conio.h>
+/*
+-include<conio.h> It is a header file used in c and cpp and it includes inbuilt functions like getch() and clrscr().It stand for console input ouput i.e. it takes input from keyboard and displays it on screen
+Q.What is getch () for?
+A.getch() method pauses the Output Console until a key is pressed. It does not use any Q. to store the input character. The entered character is immediately returned without waiting for the enter key. ... The getch() method can be used to accept hidden inputs like password, ATM pin numbers, etc.
+Q.What is Clrscr () in C ++?
+A.Up vote 1. you can use the system cls command to clear the output screen.. clrscr() is from turbo c++, inside conio.h and conio.h is "non standard", and as such should be probably avoided. example #include<windows.h> main() { some code....; system("cls"); some more code; }
+*/
+#include<dos.h>
+/*
+Q.What is include DOS h in C?
+A.h is a header file of C Language. This library has functions that are used for handling interrupts, producing sound, date and time functions, etc. It is Borland specific and works in compilers like Turbo C Compiler.
+*/
+#include <windows.h>
+/*
+- <windows. h> header file is used to access the Win32 API functions and it makes it easier for the user to use the in-built functionality. - The header file in particular includes the library and functions used in the libraries like stdio. h or stdlib.
+Q.What is stdio used for?
+A.stdio. h is a header file which has the necessary information to include the input/output related functions in our program. Example printf, scanf etc. If we want to use printf or scanf function in our program, we should include the stdio.
+Q.What is Stdlib used for?
+A.h is the header of the general purpose standard library of C programming language which includes functions involving memory allocation, process control, conversions and others.
+*/
+#include <time.h>
+/*
+Q.What is #include time H?
+A.The time. h header file contains definitions of functions to get and manipulate date and time information. It describes three time-related data types. clock_t: clock_t represents the date as an integer which is a part of the calendar time.
+*/
+
+#define SCREEN_WIDTH 90
+#define SCREEN_HEIGHT 26
+#define WIN_WIDTH 70 
+
+using namespace std; 
+ 
+HANDLE console = GetStdHandle(STD_OUTPUT_HANDLE);
+COORD CursorPosition;
+
+int enemyY[3];
+int enemyX[3];
+int enemyFlag[3];
+char car[4][4] = { ' ','±','±',' ', 
+					'±','±','±','±', 
+					' ','±','±',' ',
+					'±','±','±','±' }; 
+					
+int carPos = WIN_WIDTH/2;
+int score = 0; 
+
+void gotoxy(int x, int y){
+	CursorPosition.X = x;
+	CursorPosition.Y = y;
+	SetConsoleCursorPosition(console, CursorPosition);
+}
+void setcursor(bool visible, DWORD size) {
+	if(size == 0)
+		size = 20;	
+	
+	CONSOLE_CURSOR_INFO lpCursor;	
+	lpCursor.bVisible = visible;
+	lpCursor.dwSize = size;
+	SetConsoleCursorInfo(console,&lpCursor);
+}
+void drawBorder(){  
+	for(int i=0; i<SCREEN_HEIGHT; i++){
+		for(int j=0; j<17; j++){
+			gotoxy(0+j,i); cout<<"±";
+			gotoxy(WIN_WIDTH-j,i); cout<<"±";
+		}
+	} 
+	for(int i=0; i<SCREEN_HEIGHT; i++){
+		gotoxy(SCREEN_WIDTH,i); cout<<"±";
+	} 
+}
+void genEnemy(int ind){
+	enemyX[ind] = 17 + rand()%(33);  
+}
+void drawEnemy(int ind){
+	if( enemyFlag[ind] == true ){
+		gotoxy(enemyX[ind], enemyY[ind]);   cout<<"****";  
+		gotoxy(enemyX[ind], enemyY[ind]+1); cout<<" ** "; 
+		gotoxy(enemyX[ind], enemyY[ind]+2); cout<<"****"; 
+		gotoxy(enemyX[ind], enemyY[ind]+3); cout<<" ** ";  
+	} 
+}
+void eraseEnemy(int ind){
+	if( enemyFlag[ind] == true ){
+		gotoxy(enemyX[ind], enemyY[ind]); cout<<"    ";  
+		gotoxy(enemyX[ind], enemyY[ind]+1); cout<<"    "; 
+		gotoxy(enemyX[ind], enemyY[ind]+2); cout<<"    "; 
+		gotoxy(enemyX[ind], enemyY[ind]+3); cout<<"    "; 
+	} 
+}
+void resetEnemy(int ind){
+	eraseEnemy(ind);
+	enemyY[ind] = 1;
+	genEnemy(ind);
+}
+
+void drawCar(){
+	for(int i=0; i<4; i++){
+		for(int j=0; j<4; j++){
+			gotoxy(j+carPos, i+22); cout<<car[i][j];
+		}
+	}
+}
+void eraseCar(){
+	for(int i=0; i<4; i++){
+		for(int j=0; j<4; j++){
+			gotoxy(j+carPos, i+22); cout<<" ";
+		}
+	}
+}
+ 
+int collision(){
+	if( enemyY[0]+4 >= 23 ){
+		if( enemyX[0] + 4 - carPos >= 0 && enemyX[0] + 4 - carPos < 9  ){
+			return 1;
+		}
+	}
+	return 0;
+} 
+void gameover(){
+	system("cls");
+	cout<<endl;
+	cout<<"\t\t--------------------------"<<endl;
+	cout<<"\t\t-------- Game Over -------"<<endl;
+	cout<<"\t\t--------------------------"<<endl<<endl;
+	cout<<"\t\tPress any key to go back to menu.";
+	getch();
+}
+void updateScore(){
+	gotoxy(WIN_WIDTH + 7, 5);cout<<"Score: "<<score<<endl;
+}
+
+void instructions(){
+	
+	system("cls");
+	cout<<"Instructions";
+	cout<<"\n----------------";
+	cout<<"\n Avoid Cars by moving left or right. ";
+	cout<<"\n\n Press 'a' to move left";
+	cout<<"\n Press 'd' to move right";
+	cout<<"\n Press 'escape' to exit";
+	cout<<"\n\nPress any key to go back to menu";
+	getch();
+}
+
+void play(){
+	carPos = -1 + WIN_WIDTH/2;
+	score = 0;
+	enemyFlag[0] = 1;
+	enemyFlag[1] = 0;
+	enemyY[0] = enemyY[1] = 1;
+	  
+	system("cls"); 
+	drawBorder(); 
+	updateScore();
+	genEnemy(0);
+	genEnemy(1);
+	
+	gotoxy(WIN_WIDTH + 7, 2);cout<<"Car Game";
+	gotoxy(WIN_WIDTH + 6, 4);cout<<"----------";
+	gotoxy(WIN_WIDTH + 6, 6);cout<<"----------";
+	gotoxy(WIN_WIDTH + 7, 12);cout<<"Control ";
+	gotoxy(WIN_WIDTH + 7, 13);cout<<"-------- ";
+	gotoxy(WIN_WIDTH + 2, 14);cout<<" A Key - Left";
+	gotoxy(WIN_WIDTH + 2, 15);cout<<" D Key - Right"; 
+	
+	gotoxy(18, 5);cout<<"Press any key to start";
+	getch();
+	gotoxy(18, 5);cout<<"                      ";
+	
+	while(1){
+		if(kbhit()){
+			char ch = getch();
+			if( ch=='a' || ch=='A' ){
+				if( carPos > 18 )
+					carPos -= 4;
+			}
+			if( ch=='d' || ch=='D' ){
+				if( carPos < 50 )
+					carPos += 4;
+			} 
+			if(ch==27){
+				break;
+			}
+		} 
+		
+		drawCar(); 
+		drawEnemy(0); 
+		drawEnemy(1); 
+		if( collision() == 1  ){
+			gameover();
+			return;
+		} 
+		Sleep(50);
+		eraseCar();
+		eraseEnemy(0);
+		eraseEnemy(1);   
+		
+		if( enemyY[0] == 10 )
+			if( enemyFlag[1] == 0 )
+				enemyFlag[1] = 1;
+		
+		if( enemyFlag[0] == 1 )
+			enemyY[0] += 1;
+		
+		if( enemyFlag[1] == 1 )
+			enemyY[1] += 1;
+		 
+		if( enemyY[0] > SCREEN_HEIGHT-4 ){
+			resetEnemy(0);
+			score++;
+			updateScore();
+		}
+		if( enemyY[1] > SCREEN_HEIGHT-4 ){
+			resetEnemy(1);
+			score++;
+			updateScore();
+		}
+	}
+}
+
+int main()
+{
+	setcursor(0,0); 
+	srand( (unsigned)time(NULL)); 
+	 
+	do{
+		system("cls");
+		gotoxy(10,5); cout<<" -------------------------- "; 
+		gotoxy(10,6); cout<<" |        Car Game        | "; 
+		gotoxy(10,7); cout<<" --------------------------";
+		gotoxy(10,9); cout<<"1. Start Game";
+		gotoxy(10,10); cout<<"2. Instructions";	 
+		gotoxy(10,11); cout<<"3. Quit";
+		gotoxy(10,13); cout<<"Select option: ";
+		char op = getche();
+		
+		if( op=='1') play();
+		else if( op=='2') instructions();
+		else if( op=='3') exit(0);
+		
+	}while(1);
+	
+	return 0;
+}
